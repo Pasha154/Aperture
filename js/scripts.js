d6abd368513d7912557b21140e50589e25044322
Pasha154 <pavel154k@gmail.com>
2023-07-30 00:37:31 +0300

    aperturee

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        loop:false,
         autoplay:false,
         autoplayTimeout:2000,
 
         navText: [ '', ' ' ],
-        nav: true,
-        dots: true,
+        nav: false,
+        dots: false,
         responsive:{
             0: {
                 items: 1
@@ -33,4 +33,49 @@ $(document).ready(function(){
             $( "#dialog" ).dialog( "open" );
         });
     } );
+
+
+    /* Please ❤ this if you like it! */
+
+
+    (function($) { "use strict";
+
+
+
+        var app = function () {
+            var body = undefined;
+            var menu = undefined;
+            var menuItems = undefined;
+            var init = function init() {
+                body = document.querySelector('body');
+                menu = document.querySelector('.menu-icon');
+                menuItems = document.querySelectorAll('.nav__list-item');
+                applyListeners();
+            };
+            var applyListeners = function applyListeners() {
+                menu.addEventListener('click', function () {
+                    return toggleClass(body, 'nav-active');
+                });
+            };
+            var toggleClass = function toggleClass(element, stringClass) {
+                if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+            };
+            init();
+        }();
+
+
+        //Switch light/dark
+
+        $("#switch").on('click', function () {
+            if ($("body").hasClass("light")) {
+                $("body").removeClass("light");
+                $("#switch").removeClass("switched");
+            }
+            else {
+                $("body").addClass("light");
+                $("#switch").addClass("switched");
+            }
+        });
+
+    })(jQuery);
 });
